bc1e3ee9148571613910938c382c9c696c1efe7e
LucasChanquia <lucaschanquia@gmail.com>
2023-08-31 17:48:31 -0300

    fix v2.0

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -4,48 +4,81 @@ const {Op} = require ('sequelize');
 const axios = require ('axios');
 
 
+const getAllCountries = async () => {
+  const dbCountries = await Country.findAll();
 
-const getAllCountries = async () =>{
+  if (dbCountries.length === 0) {
+    const urlApi = await axios.get('http://localhost:5000/countries');
+    const infoApi = urlApi.data.map((e) => ({
+      id: e.cca3,
+      name: e.name.common,
+      image: e.flags.svg,
+      continent: e.continents[0],
+      capital: e.capital ? e.capital[0] : 'Not Found',
+      subregion: e.subregion || 'Not Found',
+      area: e.area,
+      population: e.population,
+    }));
 
-    const dbCountries = Country.findAll()
+    await Country.bulkCreate(infoApi, { ignoreDuplicates: true });
+  }
 
-      if(dbCountries.length === 0){
-       const urlApi = await axios.get('http:localhost:5000/countries')
-       const infoApi = await urlApi.data.map((e)=>{
-         return {
-           id: e.cca3,
-           name: e.name.common,
-           image: e.flags.svg,
-           continent: e.continents[0],
-           capital: e.capital ? e.capital[0] : 'Not Found',
-           subregion: e.subregion ? e.subregion: 'Not Found',
-           area: e.area,
-           population: e.population,
-         }
-       });
-
-       await Country.bulkCreate(infoApi, { ignoreDuplicates: true });
-  
-
-      //  for (let i = 0; i < infoApi.length; i++) {
-      //    await Country.findOrCreate({ 
-      //      where: {name: infoApi[i].name}, 
-      //      defaults: infoApi[i],
-      //    })
-      //  }
-     }
-
-   const dbCountry =  await Country.findAll({
+  const dbCountry = await Country.findAll({
     include: {
       model: Activity,
-      attributes: ["name","difficulty","duration","season"],
+      attributes: ["name", "difficulty", "duration", "season"],
       through: {
         attributes: []
       }
     }
- });
-   return dbCountry;
-} 
+  });
+
+  return dbCountry;
+}
+
+
+
+// const getAllCountries = async () =>{
+
+//     const dbCountries = Country.findAll()
+
+//       if(dbCountries.length === 0){
+//        const urlApi = await axios.get('http:localhost:5000/countries')
+//        const infoApi = await urlApi.data.map((e)=>{
+//          return {
+//            id: e.cca3,
+//            name: e.name.common,
+//            image: e.flags.svg,
+//            continent: e.continents[0],
+//            capital: e.capital ? e.capital[0] : 'Not Found',
+//            subregion: e.subregion ? e.subregion: 'Not Found',
+//            area: e.area,
+//            population: e.population,
+//          }
+//        });
+
+//        await Country.bulkCreate(infoApi);
+  
+
+//       //  for (let i = 0; i < infoApi.length; i++) {
+//       //    await Country.findOrCreate({ 
+//       //      where: {name: infoApi[i].name}, 
+//       //      defaults: infoApi[i],
+//       //    })
+//       //  }
+//      }
+
+//    const dbCountry =  await Country.findAll({
+//     include: {
+//       model: Activity,
+//       attributes: ["name","difficulty","duration","season"],
+//       through: {
+//         attributes: []
+//       }
+//     }
+//  });
+//    return dbCountry;
+// } 
 
 const getCountryById = async (id) => {
 
